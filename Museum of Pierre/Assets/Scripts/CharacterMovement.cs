c951745ebc976d55d6f600d05f0568ae6fce5e33
Connor (Home) <10873383@my.uvu.edu>
2020-02-03 00:33:08 -0700

    First two rooms Camera Setup and Character Controller

--- Museum of Pierre/Assets/Scripts/CharacterMovement.cs
+++ Museum of Pierre/Assets/Scripts/CharacterMovement.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10f, orientSpeed = 10f, jumpSpeed = 18f, gravity = 9f;
+
+    private CharacterController _controller;
+    private Vector3 _pos;
+    private Vector3 _orientation;
+
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _pos.Set(0, +jumpSpeed, 0);
+        }
+        else
+        {
+            _pos.Set(0, -gravity, Input.GetAxis("Vertical") * speed);
+        }
+        _orientation.Set(0, Input.GetAxis("Horizontal") * orientSpeed, 0);
+
+        _pos = _controller.transform.TransformDirection(_pos);
+        _controller.transform.Rotate(_orientation * Time.deltaTime);
+        _controller.Move(_pos * Time.deltaTime);
     }
 }
