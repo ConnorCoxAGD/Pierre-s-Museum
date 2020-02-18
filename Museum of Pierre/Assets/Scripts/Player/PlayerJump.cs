f19d24ee86eaf99200e50e84e15caa303b5a5c30
Connor Cox <10873383@my.uvu.edu>
2020-02-18 14:57:47 -0700

    New Approach

---
+++ Museum of Pierre/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterJump : MonoBehaviour
+{
+    public float jumpSpeed = 100, gravity = 1;
+    private CharacterController _controller;
+    private Vector3 _pos;
+
+    public void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    public void Update()
+    {
+        if (_controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _pos.y += jumpSpeed;
+        }
+        else
+        {
+            _pos.y -= gravity * Time.deltaTime;
+        }
+        _controller.Move(_pos * Time.deltaTime);
+    }
+}
---
+++ Museum of Pierre/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 10f, orientSpeed = 10f;
+
+    private CharacterController _controller;
+    private Vector3 _pos;
+    private Vector3 _orientation;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    { 
+        _pos.Set(0, 0, Input.GetAxis("Vertical") * speed);
+        _orientation.Set(0, Input.GetAxis("Horizontal") * orientSpeed, 0);
+
+        _pos = _controller.transform.TransformDirection(_pos);
+        _controller.transform.Rotate(_orientation * Time.deltaTime);
+        _controller.Move(_pos * Time.deltaTime);
+    }
+}
