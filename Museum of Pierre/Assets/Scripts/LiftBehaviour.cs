e930ab0c9381843e5dd03e2e27595a819fe3c865
Connor (Home) <10873383@my.uvu.edu>
2020-03-17 21:01:19 -0600

    Pick Up Object Working

---
+++ Museum of Pierre/Assets/Scripts/LiftBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public bool holdingObj = false;
+
+    private void OnTriggerStay(Collider obj)
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            obj.transform.position = target.position;
+            obj.transform.parent = target;
+            holdingObj = true;
+
+        }
+    }
+
+
+}
