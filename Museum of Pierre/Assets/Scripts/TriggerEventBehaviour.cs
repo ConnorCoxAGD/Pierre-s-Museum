89965c42f61627e0e6db8f751a9a05cc8ebe835b
Connor (Home) <10873383@my.uvu.edu>
2020-03-20 02:47:30 -0600

    Full Gray Box Layout wihtout Props

---
+++ Museum of Pierre/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent, TriggerExitEvent;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExitEvent.Invoke();
+    }
+}
