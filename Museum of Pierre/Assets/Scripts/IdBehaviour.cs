b3ec2b7e859aa1e3ed1339e8624eea6e83d91ce8
Connor (Home) <10873383@my.uvu.edu>
2020-03-24 09:39:49 -0600

    Match ID system in place

--- Museum of Pierre/Assets/Scripts/IdBehaviour.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class IdBehaviour : MonoBehaviour
-{
-    public NameId nameIDObjs;
-}
---
+++ Museum of Pierre/Assets/Scripts/Player/LiftBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class LiftBehaviour : MonoBehaviour
+{
+    public Transform target, allPickUps;
+    [CanBeNull]public GameObject pickUp;
+    [CanBeNull]public Rigidbody pickUpRb;
+    
+    private bool _holdingObj = false;
+
+    private void OnTriggerStay(Collider obj)
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            pickUp = obj.gameObject;
+            pickUpRb = obj.GetComponent<Rigidbody>();
+            pickUpRb.useGravity = false;
+            pickUpRb.isKinematic = true;
+            
+            pickUp.transform.position = target.position;
+            pickUp.transform.rotation = target.rotation;
+            pickUp.transform.parent = target;
+            
+            _holdingObj = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_holdingObj == true)
+        {
+            ThrowObj();
+        }
+    }
+
+    private void ThrowObj()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            pickUpRb.isKinematic = false;
+            pickUpRb.AddRelativeForce(0,0,250);
+            pickUp.transform.parent = allPickUps;
+            pickUpRb.useGravity = true;
+            _holdingObj = false;
+        }
+    }
+}
